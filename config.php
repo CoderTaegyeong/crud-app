9fbd28a41071ca454de928f2d6e2413a3f24eb8f
Taegyeong Kim <50683606+CoderTaegyeong@users.noreply.github.com>
2020-06-14 20:10:39 +0900

    Add files via upload

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+/* Database credentials. MySQL server with default setting.
+(user 'root' with no password) This is PDO code.*/
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'crud');
+
+//Connect to MySQL Database
+try{
+  $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME,
+  DB_USERNAME, DB_PASSWORD);
+  //Set the PDO error mode to Exception
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("ERROR: Could not connect. " . $e->getMessage());
+}
+
+?>
